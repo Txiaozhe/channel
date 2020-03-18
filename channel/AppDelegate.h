6609e804312d283ab4c3f78631a75d6d76bb5aa2
Jim Tang <txiaozhe@gmail.com>
2020-03-18 12:55:44 +0800

    Initial Commit

---
+++ channel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  channel
+//
+//  Created by tangxiaoji on 2020/3/18.
+//  Copyright © 2020 tangxiaoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ channel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  channel
+//
+//  Created by tangxiaoji on 2020/3/18.
+//  Copyright © 2020 tangxiaoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
